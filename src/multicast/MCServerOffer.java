03bb6a13fafc781e860908ba22c311eeddc2d58c
mrsvedberg <johan@svedberg.biz>
2015-05-10 16:39:15 +0200

    So fun

--- src/multicast/MCServerOffer.java
+++ src/multicast/MCServerOffer.java
@@ -20,14 +20,14 @@ public class MCServerOffer implements Runnable {
 			InetAddress ia = InetAddress.getByName("experiment.mcast.net");
 			ms.joinGroup(ia);
 			InetAddress local = InetAddress.getLocalHost();
-			String ip = local.getHostName();
+			
 			while (true) {
 				byte[] buf = new byte[65536];
 				DatagramPacket dp = new DatagramPacket(buf, buf.length);
 				ms.receive(dp);
 				String s = new String(dp.getData(), 0, dp.getLength());
 				System.out.println("Received: " + s);
-				byte[] buf2 = ip.getBytes();
+				byte[] buf2 = local.toString().getBytes();
 				DatagramPacket sp = new DatagramPacket(buf2, buf2.length,
 						dp.getAddress(), dp.getPort());
 				ms.send(sp);
